0be21672e900540ddba4027ff36b33c274e052f2
Bombshell05 <shannyve05@gmail.com>
2022-11-07 11:46:40 -0700

    Changes in the navbar and home

---
+++ budget-app/src/Menu.js
@@ -0,0 +1,21 @@
+export const Menu = [
+    {
+        title: "Home",
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-house-user', 
+    },
+    {
+        title: "Categories",
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-list', 
+    },
+    {
+        title: "MyBudget",
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-coins', 
+    },
+
+]
