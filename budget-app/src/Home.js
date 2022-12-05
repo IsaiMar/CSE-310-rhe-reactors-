06210ab4d6aadee7c4148179ef0fcf202039e1a3
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2022-12-05 12:22:33 -0700

    adding some stuff to the home page.

--- budget-app/src/Home.js
+++ budget-app/src/Home.js
@@ -6,9 +6,16 @@ export default function Home(){
     return(
         <div className="home">
             <h1>
+                Welcome to the Budget App
+            </h1>
+            <p>
+                Today you are going to learn how to make your own budget, 
+            </p>
+            <h1>
                 What is a budget?
             </h1>
             <p>
+                Budgeting is a skill everyone needs to have <br/><br/>
                 According to Oxford dictionary, "A budget is 
                 spending plan based on income and expenses. In other words, it’s an estimate of 
                 how much money you’ll make and spendover a certain period of time, such as a month or year. 
